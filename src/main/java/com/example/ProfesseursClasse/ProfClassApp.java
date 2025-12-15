0c8bb966b1f206fc1dee95027dfb7013b03fa3b9
awatef-toutou <awatefnf@hotmail.fr>
2025-12-15 13:18:29 +0100

    exercice professeurs classes avec les jpa relations

---
+++ src/main/java/com/example/ProfesseursClasse/ProfClassApp.java
@@ -0,0 +1,13 @@
+package com.example.ProfesseursClasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfClassApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfClassApp.class, args);
+	}
+
+}
